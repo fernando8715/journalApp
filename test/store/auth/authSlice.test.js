cc08540d6686cdafd3517665c27ef20651ad48f8
Fernando <fernando8715@hotmail.com>
2025-01-26 16:57:36 -0500

    pruebas en authSlice

--- test/store/auth/authSlice.test.js
+++ test/store/auth/authSlice.test.js
@@ -1,16 +1,64 @@
-import { authSlice } from "../../../src/store/auth/authSlice";
-import { initialState } from "../../fixtures/authFixtures";
+import { authSlice, checkingCredentials, login, logout } from "../../../src/store/auth/authSlice";
+import { autenticatedState, demoUser, initialState } from "../../fixtures/authFixtures";
 
 describe('prebas en el authSlice', () => {
 
     test('debe regresar el estado inicial y llamarse "auth', () => {
-        
+
         const state = authSlice.reducer(initialState, {});
-        console.log(state);
-        
-        
 
+        expect(authSlice.name).toBe('auth');
+        expect(state).toEqual(initialState);
+    });
 
-        // expect(receive).fn(expect)
+    test('debe de realizar el login', () => {
+
+        const state = authSlice.reducer(initialState, login(demoUser));
+
+        expect(state).toEqual({
+            status: 'authenticated',
+            uid: '1234fer',
+            email: 'correo@correo.co',
+            displayName: 'prueba user',
+            photoURL: 'http://prueba.jpg',
+            errorMessage: null,
+        });
+    });
+
+    test('debe realizar el logout sin argumentos', () => {
+
+        const state = authSlice.reducer(autenticatedState, logout());
+        
+        expect(state).toEqual({
+            status: 'not-authenticated',
+            uid: null,
+            email: null,
+            displayName: null,
+            photoURL: null,
+            errorMessage: undefined,
+        });    
+    });
+    
+    
+    test('debe realizar el logout con argumentos', () => {
+        
+        const errorMessage = 'credenciales no son correctas';
+        const state = authSlice.reducer(autenticatedState, logout({errorMessage}));
+
+        expect(state).toEqual({
+            status: 'not-authenticated',
+            uid: null,
+            email: null,
+            displayName: null,
+            photoURL: null,
+            errorMessage: errorMessage,
+        });
+    });
+
+    test('debe cambiar el estado a checking', () => {
+        
+        const state = authSlice.reducer(autenticatedState, checkingCredentials());
+        
+        expect(state.status).toBe('checking');     
     });
 });
